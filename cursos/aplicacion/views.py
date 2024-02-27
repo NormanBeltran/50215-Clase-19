f53941e77d68d617af6d81b98ac0ddb0c80e7d66
NormanBeltran <josenormanbeltran@gmail.com>
2024-02-27 20:49:31 -0300

    Clase 19: Proyecto Inicial con Diseño

---
+++ cursos/aplicacion/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import *
+
+# Copyright Norman Beltran
+
+def home(request):
+    return render(request, "aplicacion/index.html") 
+
+def cursos(request):
+    contexto = {'cursos': Curso.objects.all()}
+    return render(request, "aplicacion/cursos.html", contexto) 
+
+def profesores(request):
+    return render(request, "aplicacion/profesores.html") 
+
+def estudiantes(request):
+    return render(request, "aplicacion/estudiantes.html") 
+
+def entregables(request):
+    return render(request, "aplicacion/entregables.html") 
